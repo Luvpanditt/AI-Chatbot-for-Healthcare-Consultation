87315becf9be7c00a421bf50d4a3118c375737a8
Luv Pandit <Luvinterv@gmail.com>
2025-04-02 11:24:02 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Healthcare Consultation',
+    version= '0.0.0',
+    author= 'Luv Pandit',
+    author_email= 'Luvinterv@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
